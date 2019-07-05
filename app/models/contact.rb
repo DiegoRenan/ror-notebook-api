a492dcb171a0f09d3786a2d5294f7ec727be2b48
DiegoRenan <renan.ccomp@gmail.com>
2019-07-05 09:24:42 -0300

    render JSON with associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,21 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+  
   def author
     "Jackson Pires"
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   #render sobreescrevendo o as_json. Aplicado para todas actions
   def as_json(options={})
-    super(methods: :author, root: true)
+    super(
+      methods: [:author, :kind_description], 
+      root: true,
+      #include: :kind
+      #include: { kind: { only: :description } }
+    )
   end
 end
